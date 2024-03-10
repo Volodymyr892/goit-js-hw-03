09155278f76c4f803e03a433afddac4f59a300d2
Volodymyr892 <114510313+Volodymyr892@users.noreply.github.com>
2024-03-10 09:16:58 +0200

    fixed

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function makeArray(firstArray, secondArray, maxLength) {
     const allClients = firstArray.concat(secondArray);
     const len = allClients.length;
-        if(len => maxLength) {
+        if(len >= maxLength) {
             return allClients.slice(0, maxLength);
         } else {
             return len;
@@ -16,13 +16,13 @@ console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)) 
 
 
 
-function makeArray(firstArray, secondArray, maxLength) {
-    const allClients = firstArray.concat(secondArray);
-    return allClients.slice(0, maxLength);
-}
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)) //повертає ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4))// повертає ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3))// повертає ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)) //повертає ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4))// повертає ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)) //повертає []
+// function makeArray(firstArray, secondArray, maxLength) {
+//     const allClients = firstArray.concat(secondArray);
+//     return allClients.slice(0, maxLength);
+// }
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)) //повертає ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4))// повертає ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3))// повертає ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)) //повертає ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4))// повертає ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)) //повертає []
